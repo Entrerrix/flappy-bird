71f6770772a4f31c82d6dd739a7593d40532baf0
Entrerrix <d.barcelo.costa@gmail.com>
2024-03-24 10:40:34 +0000

    2

--- src/index.js
+++ src/index.js
@@ -1,46 +1,77 @@
-import Phaser from 'phaser';
 
-const config ={
+import Phaser from 'phaser';
+const config = {
+  // WebGL (Web graphics library) JS Api for rendering 2D and 3D graphics
   type: Phaser.AUTO,
-  height:600,
-  width:800,
+  width: 800,
+  height: 600,
   physics: {
+    // Arcade physics plugin, manages physics simulation
     default: 'arcade',
-    debug: true,
     arcade: {
-    gravity: {y: 200}
-   }
+      debug: true,
+    }
   },
   scene: {
     preload,
     create,
     update
   }
-};
+}
+
+const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
+
+let bird = null;
+
+let upperPipe = null;
+let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
+const pipeVerticalDistanceRange = [150, 250];
+
+
+const flapVelocity = 250;
+const initalBirdPosition = {x: config.width * 0.1, y: config.height / 2}
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-
+  this.load.image('pipe', 'assets/pipe.png');
 }
-
-const VELOCITY = 500;
-
-let bird = null;
-let  totalDelta = null;
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(config.width * 0.5 / 2, config.height / 2, 'bird').setOrigin(0)
-  bird.body.velocity.x = VELOCITY;
-}
+  bird = this.physics.add.sprite(initalBirdPosition.x, initalBirdPosition.y, 'bird').setOrigin(0);
+  bird.body.gravity.y = 400;
 
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
 
-  function update(time, delta) {
-    if(bird.x >= config.width - bird.width) {
-     bird.body.velocity.x = -VELOCITY;
-    } else if (bird.x <= 0) {
-      bird.body.velocity.x = VELOCITY
-    }
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
   }
 
-new Phaser.Game(config);
 
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
+}
+// if bird y position is small than 0 or greater than height of the canvas
+// then alert "you have lost"
+function update(time, delta) {
+ if (bird.y > config.height || bird.y < -bird.height) {
+  restartBirdPosition();
+ }
+}
+function restartBirdPosition() {
+  bird.x = initalBirdPosition.x;
+  bird.y = initalBirdPosition.y;
+  bird.body.velocity.y = 0;
+}
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
+}
+new Phaser.Game(config);
